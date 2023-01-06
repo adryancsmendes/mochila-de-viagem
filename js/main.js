2ed9b1f1dec757a7f0539dcb49d68d5989d92f47
Adryan Mendes <adryanmendes2014@gmail.com>
2023-01-06 11:47:39 -0300

    Adicionando funcionalidade para salvar cada novo item no localStorage

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
+const itens = [];
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();//previnimos o comportamento padrão de atualizar a página ao clicarmos em submit
 
-    criaElemento(event.target.elements['nome'].value,event.target.elements['quantidade'].value);
+    const nome = event.target.elements['nome'].value;//capturamos o valor do campo nome do formulário
+    const quantidade = event.target.elements['quantidade'].value;//capturamos o valor do campo quantidade do formulário
+
+    criaElemento(nome,quantidade);
+    
+    form.reset();//após o envio do formulário limpamos os campos dele
 });
 
-function criaElemento(nome, quantidade){
+function criaElemento(nome,quantidade){
 
     const novoItem = document.createElement('li');//criamos um novo elemento li que irá fazer parte de nossa lista
     novoItem.classList.add('item');//adicionamos a classe 'item' ao elemento
@@ -20,4 +26,12 @@ function criaElemento(nome, quantidade){
 
     lista.appendChild(novoItem);//fazemos com que nossa lista aglutine o novo item li
 
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }//criamos uma const que recebe um par de objetos
+
+    itens.push(itemAtual);//empurramos para dentro de nosso array itens cada novo item que estamos adicionando
+
+    localStorage.setItem("item", JSON.stringify(itens));//passamos como valor da chave do item que irá para o local storage nosso array de itens criados
 }
